2274dee47e829bd0ffddad8c1bb5efe34f3d89c6
Shafin098 <shafinashraf074@gmail.com>
2018-05-27 13:31:16 +0600

    added realtime functionality

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 let stocks = []
 
+
 app.set('view engine', 'ejs');
 
 app.get('/', function (req,res) {
@@ -15,33 +16,69 @@ app.get('/', function (req,res) {
 
 	}
 	res.render('index', {data:stocks});
-	// body...
 })
 
-app.listen(3000, function() {
+app.get('/data', function (req,res) {
+	res.json(stocks);
+})
+
+app.use(function(req,res){
+    res.render('error');
+});
+
+app.listen(process.env.PORT || 3000, function() {
 	console.log('started');
 });
 
 let url ='http://www.dsebd.org/dseX_share.php';
 
 request(url, function (error, response, body) {
-if (error) {
+	if (error) {
 	console.log(error)
-}
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  const $ = cheerio.load(body);
-  let stock_name = []
-  let data;
-  $('a.abhead').each(function(i, elem) {
+	}
+	console.log('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	const $ = cheerio.load(body);
+	let stock_name = []
+    let data;
+    $('a.abhead').each(function(i, elem) {
   	stock_name[i] = $(this).text();
   	data = stock_name[i].trim().split(/\s+/);
   	stocks.push(data)
-});
-let final_data = {};
-for (var i = data.length - 1; i >= 0; i--) {
-	final_data.stock = data[i];
-}
-console.log(stocks[0]);
-});
+	});
+	let final_data = {};
+	if (data != undefined) {
+	for (var i = data.length - 1; i >= 0; i--) {
+		final_data.stock = data[i];
+		}
+	}
+	console.log(stocks.length);
+	});
 
+let refresh_data = function() { 
+	stocks.length = 0;
+	request(url, function (error, response, body) {
+	if (error) {
+	console.log(error)
+	}
+	console.log('error:', error); // Print the error if one occurred
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+	const $ = cheerio.load(body);
+	let stock_name = []
+    let data;
+    $('a.abhead').each(function(i, elem) {
+  	stock_name[i] = $(this).text();
+  	data = stock_name[i].trim().split(/\s+/);
+  	stocks.push(data);
+	});
+	let final_data = {};
+	if (data != undefined) {
+	for (var i = data.length - 1; i >= 0; i--) {
+		final_data.stock = data[i];
+		}
+	}
+	console.log(stocks[6]);
+	});
+}
+
+setInterval(refresh_data, 20000);
